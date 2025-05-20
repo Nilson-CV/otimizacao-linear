6fc9e48fe4824f2220518184148d65cb1c52729e
Nilson-CV <68797329+Nilson-CV@users.noreply.github.com>
2025-05-20 23:02:53 +0000

    Atualiza app com visualização de PDF e melhorias visuais

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 main_page = st.Page("app.py", title="Cursos", icon="📚")
-page_2 = st.Page("1_Análise Matemática I.py", title="Análise Matemática I", icon="❄️📘 ")
+page_2 = st.Page("AnáliseMatemáticaI.py", title="Análise Matemática I", icon="❄️📘 ")
 
 pg = st.navigation([main_page, page_2])
 
